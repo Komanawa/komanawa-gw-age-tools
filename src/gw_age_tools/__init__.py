df28e24f6e52edc8e3a9198cb94ca4dc463ee67b
matt_dumont <hansonmcoombs@gmail.com>
2023-10-17 17:29:29 +1300

    add a lightweight predict future function

--- src/gw_age_tools/__init__.py
+++ src/gw_age_tools/__init__.py
@@ -5,4 +5,4 @@ on: 22/09/23
 from gw_age_tools.exponential_piston_flow import exponential_piston_flow_cdf, binary_exp_piston_flow_cdf, \
     binary_exp_piston_flow, exponential_piston_flow, make_age_dist, check_age_inputs
 from gw_age_tools.source_predictions import estimate_source_conc_bepfm, predict_future_conc_bepm, \
-    predict_source_future_past_conc_bepm, predict_historical_source_conc
+    predict_source_future_past_conc_bepm, predict_historical_source_conc, lightweight_predict_future
